1ea217b68361389c0d0a9b299cea913211957f5d
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-12 18:09:46 +0200

    feat: extend road graph to hostile countryside territories

--- Source/GTT/Private/World/GTTRoadGraph.cpp
+++ Source/GTT/Private/World/GTTRoadGraph.cpp
@@ -51,7 +51,10 @@ void FGTTRoadGraph::BuildGraph(TArray<FGTTRoadNode>& OutNodes)
         {TEXT("ForestDeep"), TEXT("FOREST DEEP"), FVector(7050,-950,100), {}},
         {TEXT("WardenOutpost"), TEXT("WARDEN OUTPOST"), FVector(5050,700,100), {}},
         {TEXT("WoodYardGate"), TEXT("WOOD YARD GATE"), FVector(7850,900,100), {}},
-        {TEXT("LakeEast"), TEXT("LAKE EAST"), FVector(6100,-500,100), {}}
+        {TEXT("LakeEast"), TEXT("LAKE EAST"), FVector(6100,-500,100), {}},
+        {TEXT("ScrapYardRoad"), TEXT("RUST DOGS SCRAP YARD ROAD"), FVector(-5050,-450,100), {}},
+        {TEXT("OldQuarryRoad"), TEXT("STONE CROWS OLD QUARRY ROAD"), FVector(8600,2850,100), {}},
+        {TEXT("MarshCampRoad"), TEXT("MUD JACKALS MARSH CAMP ROAD"), FVector(6900,-3300,100), {}}
     };
 
     Link(OutNodes,0,1); Link(OutNodes,1,2); Link(OutNodes,2,3); Link(OutNodes,3,4);
@@ -59,6 +62,7 @@ void FGTTRoadGraph::BuildGraph(TArray<FGTTRoadNode>& OutNodes)
     Link(OutNodes,4,8); Link(OutNodes,8,9); Link(OutNodes,9,15); Link(OutNodes,15,13); Link(OutNodes,13,5);
     Link(OutNodes,3,12); Link(OutNodes,12,19); Link(OutNodes,19,16); Link(OutNodes,16,10); Link(OutNodes,10,17);
     Link(OutNodes,17,11); Link(OutNodes,11,18); Link(OutNodes,18,15); Link(OutNodes,5,14); Link(OutNodes,14,6);
+    Link(OutNodes,7,20); Link(OutNodes,11,21); Link(OutNodes,16,22); Link(OutNodes,19,22);
 }
 
 int32 FGTTRoadGraph::FindClosestNode(const FVector& Location)
