74e0587ee0e7cc0493a7e9c0b7359155ea3b5f49
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-12 15:10:11 +0200

    feat(combat): add civilian combat reactions and health

--- Source/GTT/Public/NPC/GTTCitizenPawn.h
+++ Source/GTT/Public/NPC/GTTCitizenPawn.h
@@ -14,15 +14,22 @@ public:
     virtual void BeginPlay() override;
     virtual void Tick(float DeltaSeconds) override;
     UFUNCTION(BlueprintCallable, Category="GTT|NPC|Crime") bool TryWitnessVehicleTheft(AGTTVehicleBase* Vehicle, APawn* Offender);
+    UFUNCTION(BlueprintCallable, Category="GTT|NPC|Combat") void ApplyCombatHit(float Damage, const FVector& HitDirection, float Knockback, APawn* Attacker);
+    UFUNCTION(BlueprintPure, Category="GTT|NPC|Combat") float GetHealthPercent() const { return MaxHealth > 0.0f ? Health / MaxHealth : 0.0f; }
+    UFUNCTION(BlueprintPure, Category="GTT|NPC|Combat") bool IsKnockedOut() const { return bKnockedOut; }
 protected:
     void ChooseNewWanderTarget();
     FVector GetScheduleCenter() const;
+    void UpdateCombatBehavior(float DeltaSeconds);
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GTT|NPC") TObjectPtr<UStaticMeshComponent> BodyMesh;
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GTT|NPC") TObjectPtr<UStaticMeshComponent> HeadMesh;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|NPC|Crime", meta=(ClampMin="100.0")) float WitnessRadius = 1800.0f;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|NPC|Crime", meta=(ClampMin="0.0")) float WitnessHeat = 9.0f;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|NPC|Movement", meta=(ClampMin="100.0")) float WanderRadius = 750.0f;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|NPC|Movement", meta=(ClampMin="0.0")) float WanderSpeed = 135.0f;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|NPC|Combat") float MaxHealth = 100.0f;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|NPC|Combat") float RetaliationDistance = 175.0f;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|NPC|Combat") float RetaliationDamage = 7.0f;
 private:
     FVector HomeLocation = FVector::ZeroVector;
     FVector WorkLocation = FVector::ZeroVector;
@@ -32,4 +39,9 @@ private:
     float RetargetTimeRemaining = 0.0f;
     TWeakObjectPtr<AGTTVehicleBase> LastWitnessedVehicle;
     TWeakObjectPtr<AGTTDayNightCycle> DayNightCycle;
+    TWeakObjectPtr<APawn> CombatTarget;
+    float Health = 100.0f;
+    float CombatCooldown = 0.0f;
+    float KnockoutTimeRemaining = 0.0f;
+    bool bKnockedOut = false;
 };
