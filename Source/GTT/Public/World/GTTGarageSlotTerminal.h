204b1891043c0a57edff7544e95cd37d9285410d
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-12 11:22:17 +0200

    feat: add explicit garage fleet slot selectors

---
+++ Source/GTT/Public/World/GTTGarageSlotTerminal.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interaction/GTTInteractable.h"
+#include "GTTGarageSlotTerminal.generated.h"
+
+class UStaticMeshComponent;
+class UTextRenderComponent;
+class AGTTVehicleBase;
+
+UCLASS()
+class GTT_API AGTTGarageSlotTerminal : public AActor, public IGTTInteractable
+{
+    GENERATED_BODY()
+
+public:
+    AGTTGarageSlotTerminal();
+    virtual void BeginPlay() override;
+    virtual void Tick(float DeltaSeconds) override;
+    virtual void Interact_Implementation(AActor* Interactor) override;
+    virtual FText GetInteractionText_Implementation() const override;
+
+    UFUNCTION(BlueprintCallable, Category="GTT|Garage")
+    void SetSlotIndex(int32 InSlotIndex);
+
+    UFUNCTION(BlueprintPure, Category="GTT|Garage")
+    int32 GetSlotIndex() const { return SlotIndex; }
+
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GTT|Garage")
+    TObjectPtr<UStaticMeshComponent> Mesh;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GTT|Garage")
+    TObjectPtr<UTextRenderComponent> Label;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|Garage", meta=(ClampMin="0"))
+    int32 RecallServiceCost = 15;
+
+private:
+    AGTTVehicleBase* ResolveSlotVehicle() const;
+    void RefreshLabel();
+    int32 GetVehicleSortPriority(const AGTTVehicleBase* Vehicle) const;
+
+    int32 SlotIndex = 0;
+    float RefreshClock = 0.0f;
+};
