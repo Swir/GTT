324a70d542416984c27405d6ff0c46ac97b51358
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-12 15:11:21 +0200

    feat(combat): add player health and defeat loop

--- Source/GTT/Public/Combat/GTTCombatComponent.h
+++ Source/GTT/Public/Combat/GTTCombatComponent.h
@@ -12,14 +12,17 @@ class GTT_API UGTTCombatComponent : public UActorComponent
 
 public:
     UGTTCombatComponent();
+    virtual void BeginPlay() override;
     virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
     UFUNCTION(BlueprintCallable, Category="GTT|Combat") bool AddWeapon(EGTTWeaponType Type, int32 Ammo = 0, bool bAutoEquip = true);
     UFUNCTION(BlueprintCallable, Category="GTT|Combat") void Attack();
     UFUNCTION(BlueprintCallable, Category="GTT|Combat") void CycleWeapon();
     UFUNCTION(BlueprintCallable, Category="GTT|Combat") void DropCurrentWeapon();
+    UFUNCTION(BlueprintCallable, Category="GTT|Combat") void ApplyIncomingDamage(float Damage, const FString& SourceLabel);
     UFUNCTION(BlueprintPure, Category="GTT|Combat") EGTTWeaponType GetEquippedWeapon() const { return EquippedWeapon; }
     UFUNCTION(BlueprintPure, Category="GTT|Combat") int32 GetShotgunAmmo() const { return ShotgunAmmo; }
+    UFUNCTION(BlueprintPure, Category="GTT|Combat") float GetHealthPercent() const { return MaxHealth > 0.0f ? Health / MaxHealth : 0.0f; }
     UFUNCTION(BlueprintPure, Category="GTT|Combat") FString GetCombatStatusText() const;
     UFUNCTION(BlueprintPure, Category="GTT|Combat") bool HasWeapon(EGTTWeaponType Type) const { return Inventory.Contains(Type); }
 
@@ -28,9 +31,12 @@ private:
     void PerformShotgunAttack(const FGTTWeaponProfile& Profile);
     void ApplyHit(AActor* Target, const FVector& HitDirection, const FGTTWeaponProfile& Profile);
     void AddCrimeHeat(float Amount, const FString& Message);
+    void HandleDefeat();
 
     UPROPERTY() TArray<EGTTWeaponType> Inventory;
     EGTTWeaponType EquippedWeapon = EGTTWeaponType::BareHands;
     int32 ShotgunAmmo = 0;
     float AttackCooldownRemaining = 0.0f;
+    float MaxHealth = 100.0f;
+    float Health = 100.0f;
 };
