9a1e41a76797e456522464617c93bbf77cc3b049
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-13 06:10:48 +0200

    feat: add Chaos vehicle runtime bridge interface

---
+++ Source/GTT/Public/Vehicles/GTTChaosVehicleBridgeComponent.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Vehicles/GTTChaosVehicleSpec.h"
+#include "GTTChaosVehicleBridgeComponent.generated.h"
+
+class AGTTVehicleBase;
+class UChaosWheeledVehicleMovementComponent;
+class UGTTVehicleDynamicsComponent;
+
+UENUM(BlueprintType)
+enum class EGTTChaosBridgeState : uint8
+{
+    WaitingForNativeRig,
+    NativeReady,
+    NativeDriving,
+    NativeBlocked
+};
+
+UCLASS(ClassGroup=(GTT), meta=(BlueprintSpawnableComponent))
+class GTT_API UGTTChaosVehicleBridgeComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UGTTChaosVehicleBridgeComponent();
+
+    virtual void BeginPlay() override;
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+    UFUNCTION(BlueprintCallable, Category="GTT|Vehicle|Chaos")
+    void CaptureThrottleInput(float Value);
+
+    UFUNCTION(BlueprintCallable, Category="GTT|Vehicle|Chaos")
+    void CaptureSteeringInput(float Value);
+
+    UFUNCTION(BlueprintCallable, Category="GTT|Vehicle|Chaos")
+    void RefreshNativeBinding();
+
+    UFUNCTION(BlueprintPure, Category="GTT|Vehicle|Chaos")
+    bool IsNativeMovementReady() const { return bNativeMovementReady; }
+
+    UFUNCTION(BlueprintPure, Category="GTT|Vehicle|Chaos")
+    EGTTChaosBridgeState GetBridgeState() const { return BridgeState; }
+
+    UFUNCTION(BlueprintPure, Category="GTT|Vehicle|Chaos")
+    FString GetBridgeStatusSummary() const;
+
+    UFUNCTION(BlueprintPure, Category="GTT|Vehicle|Chaos")
+    FGTTChaosVehicleSpec GetResolvedSpec() const { return ResolvedSpec; }
+
+private:
+    void ResolveSpec();
+    void UpdateRuntimeState();
+    void RouteInputsToChaos();
+    void DisableLegacyDynamicsIfNeeded();
+
+    UPROPERTY(Transient) TObjectPtr<AGTTVehicleBase> VehicleOwner;
+    UPROPERTY(Transient) TObjectPtr<UChaosWheeledVehicleMovementComponent> NativeMovement;
+    UPROPERTY(Transient) TObjectPtr<UGTTVehicleDynamicsComponent> LegacyDynamics;
+
+    UPROPERTY(VisibleInstanceOnly, Category="GTT|Vehicle|Chaos") FGTTChaosVehicleSpec ResolvedSpec;
+    UPROPERTY(VisibleInstanceOnly, Category="GTT|Vehicle|Chaos") EGTTChaosBridgeState BridgeState = EGTTChaosBridgeState::WaitingForNativeRig;
+    UPROPERTY(VisibleInstanceOnly, Category="GTT|Vehicle|Chaos") bool bNativeMovementReady = false;
+    UPROPERTY(VisibleInstanceOnly, Category="GTT|Vehicle|Chaos") float EffectivePowerScale = 1.0f;
+    UPROPERTY(VisibleInstanceOnly, Category="GTT|Vehicle|Chaos") float EffectiveGripScale = 1.0f;
+
+    float RawThrottleInput = 0.0f;
+    float RawSteeringInput = 0.0f;
+    float RebindCountdown = 0.0f;
+    bool bLegacyDynamicsDisabled = false;
+};
