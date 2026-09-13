49a5413094c40520010fb549a2e3715e6255fc1f
Wojciech <swirjestem@hotmail.com>
2026-09-13 22:13:49 +0200

    feat: expose paid heavy-haul roadside recovery

--- Source/GTT/Public/Activities/GTTHeavyHaulDirector.h
+++ Source/GTT/Public/Activities/GTTHeavyHaulDirector.h
@@ -33,11 +33,13 @@ public:
     UFUNCTION(BlueprintCallable, Category="GTT|HeavyHaul") bool TryHitchTrailer(APawn* PlayerPawn);
     UFUNCTION(BlueprintCallable, Category="GTT|HeavyHaul") bool TryLoadTimber(APawn* PlayerPawn);
     UFUNCTION(BlueprintCallable, Category="GTT|HeavyHaul") bool TryDeliverTimber(APawn* PlayerPawn);
+    UFUNCTION(BlueprintCallable, Category="GTT|HeavyHaul") bool TryRoadsideRepair(APawn* PlayerPawn);
 
     UFUNCTION(BlueprintPure, Category="GTT|HeavyHaul") EGTTHeavyHaulStage GetStage() const { return Stage; }
     UFUNCTION(BlueprintPure, Category="GTT|HeavyHaul") FString GetObjectiveText() const;
     UFUNCTION(BlueprintPure, Category="GTT|HeavyHaul") AGTTFarmTrailer* GetTrailer() const { return Trailer; }
     UFUNCTION(BlueprintPure, Category="GTT|HeavyHaul") bool IsActive() const { return Stage != EGTTHeavyHaulStage::Idle && Stage != EGTTHeavyHaulStage::Completed; }
+    UFUNCTION(BlueprintPure, Category="GTT|HeavyHaul") int32 GetRoadsideRepairCount() const { return RoadsideRepairCount; }
 
 private:
     bool CanTakeContract(APawn* PlayerPawn) const;
@@ -58,6 +60,7 @@ private:
 
     EGTTHeavyHaulStage Stage = EGTTHeavyHaulStage::Idle;
     float TimeRemaining = 0.0f;
+    int32 RoadsideRepairCount = 0;
 
     FVector TrailerYardLocation = FVector(-3200.0f, -1500.0f, 95.0f);
     FVector WoodYardLoadLocation = FVector(7850.0f, 450.0f, 80.0f);
@@ -66,4 +69,6 @@ private:
     UPROPERTY(EditDefaultsOnly, Category="GTT|HeavyHaul") float ContractTimeLimit = 330.0f;
     UPROPERTY(EditDefaultsOnly, Category="GTT|HeavyHaul") int32 BaseReward = 900;
     UPROPERTY(EditDefaultsOnly, Category="GTT|HeavyHaul") int32 FastBonus = 250;
+    UPROPERTY(EditDefaultsOnly, Category="GTT|HeavyHaul|Recovery") int32 RoadsideRepairBaseCost = 140;
+    UPROPERTY(EditDefaultsOnly, Category="GTT|HeavyHaul|Recovery") int32 RoadsideRepairEscalation = 90;
 };
