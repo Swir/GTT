2a707d2b33dfb26f4df81bc696ded58b28a11005
Wojciech <swirjestem@hotmail.com>
2026-09-15 14:08:43 +0200

    feat: expose deterministic Native Chaos control hook

--- Source/GTT/Public/Vehicles/GTTRoadVehicleNativePawn.h
+++ Source/GTT/Public/Vehicles/GTTRoadVehicleNativePawn.h
@@ -5,177 +5,24 @@
 #include "WheeledVehiclePawn.h"
 #include "GTTRoadVehicleNativePawn.generated.h"
 
-class AGTTVehicleBase;
-class UCameraComponent;
-class UChaosWheeledVehicleMovementComponent;
-class UPrimitiveComponent;
-class USpringArmComponent;
-class UStaticMeshComponent;
-
-UENUM(BlueprintType)
-enum class EGTTRoadDamageZone : uint8
-{
-    Front,
-    Rear,
-    Left,
-    Right
-};
-
-USTRUCT(BlueprintType)
-struct GTT_API FGTTRoadVehicleMigrationSnapshot
-{
-    GENERATED_BODY()
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite) float ConditionPercent = 1.0f;
-    UPROPERTY(EditAnywhere, BlueprintReadWrite) float FuelLiters = 0.0f;
-    UPROPERTY(EditAnywhere, BlueprintReadWrite) bool bOwnedByPlayer = false;
-    UPROPERTY(EditAnywhere, BlueprintReadWrite) int32 EngineUpgradeLevel = 0;
-    UPROPERTY(EditAnywhere, BlueprintReadWrite) int32 TireUpgradeLevel = 0;
-    UPROPERTY(EditAnywhere, BlueprintReadWrite) float TireIntegrity = 1.0f;
-};
-
-USTRUCT(BlueprintType)
-struct GTT_API FGTTRoadBodyDamageSnapshot
-{
-    GENERATED_BODY()
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly) float FrontHealth = 1.0f;
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly) float RearHealth = 1.0f;
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly) float LeftHealth = 1.0f;
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly) float RightHealth = 1.0f;
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly) float CoolingStress = 0.0f;
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly) int32 DetachedPanelCount = 0;
-};
-
-UCLASS(Abstract, Blueprintable)
-class GTT_API AGTTRoadVehicleNativePawn : public AWheeledVehiclePawn, public IGTTInteractable
-{
-    GENERATED_BODY()
-
+class AGTTVehicleBase; class UCameraComponent; class UChaosWheeledVehicleMovementComponent; class UPrimitiveComponent; class USpringArmComponent; class UStaticMeshComponent;
+UENUM(BlueprintType) enum class EGTTRoadDamageZone : uint8 { Front, Rear, Left, Right };
+USTRUCT(BlueprintType) struct GTT_API FGTTRoadVehicleMigrationSnapshot { GENERATED_BODY() UPROPERTY(EditAnywhere,BlueprintReadWrite) float ConditionPercent=1.f; UPROPERTY(EditAnywhere,BlueprintReadWrite) float FuelLiters=0.f; UPROPERTY(EditAnywhere,BlueprintReadWrite) bool bOwnedByPlayer=false; UPROPERTY(EditAnywhere,BlueprintReadWrite) int32 EngineUpgradeLevel=0; UPROPERTY(EditAnywhere,BlueprintReadWrite) int32 TireUpgradeLevel=0; UPROPERTY(EditAnywhere,BlueprintReadWrite) float TireIntegrity=1.f; };
+USTRUCT(BlueprintType) struct GTT_API FGTTRoadBodyDamageSnapshot { GENERATED_BODY() UPROPERTY(VisibleAnywhere,BlueprintReadOnly) float FrontHealth=1.f; UPROPERTY(VisibleAnywhere,BlueprintReadOnly) float RearHealth=1.f; UPROPERTY(VisibleAnywhere,BlueprintReadOnly) float LeftHealth=1.f; UPROPERTY(VisibleAnywhere,BlueprintReadOnly) float RightHealth=1.f; UPROPERTY(VisibleAnywhere,BlueprintReadOnly) float CoolingStress=0.f; UPROPERTY(VisibleAnywhere,BlueprintReadOnly) int32 DetachedPanelCount=0; };
+UCLASS(Abstract,Blueprintable) class GTT_API AGTTRoadVehicleNativePawn : public AWheeledVehiclePawn, public IGTTInteractable { GENERATED_BODY()
 public:
-    AGTTRoadVehicleNativePawn();
-
-    virtual void Tick(float DeltaSeconds) override;
-    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
-    virtual void Interact_Implementation(AActor* Interactor) override;
-    virtual FText GetInteractionText_Implementation() const override;
-    virtual void NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved,
-        FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
-
-    UFUNCTION(BlueprintCallable, Category="GTT|Chaos|Fleet") bool ConfigureAndValidateNativeRoadVehicle(FString& OutSummary);
-    UFUNCTION(BlueprintCallable, Category="GTT|Chaos|Fleet") bool TryActivateLegacyTakeover();
-    UFUNCTION(BlueprintCallable, Category="GTT|Chaos|Fleet") void DeactivateLegacyTakeover();
-    UFUNCTION(BlueprintCallable, Category="GTT|Vehicle") void ExitNativeVehicle();
-    UFUNCTION(BlueprintCallable, Category="GTT|Vehicle|Cargo") virtual void SetCargoLoadFactor(float NewLoadFactor);
-    UFUNCTION(BlueprintCallable, Category="GTT|Vehicle|Workshop") bool ApplyNativeWorkshopService();
-
-    UFUNCTION(BlueprintPure, Category="GTT|Chaos|Fleet") bool IsNativeReady() const { return bNativeReady; }
-    UFUNCTION(BlueprintPure, Category="GTT|Chaos|Fleet") bool IsLegacyTakeoverActive() const { return bTakeoverActive; }
-    UFUNCTION(BlueprintPure, Category="GTT|Vehicle|Save") FName GetPersistentVehicleId() const { return NativeVehicleId; }
-    UFUNCTION(BlueprintPure, Category="GTT|Vehicle") FText GetVehicleDisplayName() const { return NativeDisplayName; }
-    UFUNCTION(BlueprintPure, Category="GTT|Vehicle") APawn* GetDriverPawn() const { return PreviousPawn.Get(); }
-    UFUNCTION(BlueprintPure, Category="GTT|Vehicle|Cargo") float GetCargoLoadFactor() const { return CargoLoadFactor; }
-    UFUNCTION(BlueprintPure, Category="GTT|Chaos|Migration") FGTTRoadVehicleMigrationSnapshot GetMigrationSnapshot() const { return MigrationSnapshot; }
-    UFUNCTION(BlueprintPure, Category="GTT|Chaos|Runtime") float GetRuntimeWheelRisk() const { return RuntimeWheelRisk; }
-    UFUNCTION(BlueprintPure, Category="GTT|Chaos|Runtime") int32 GetRuntimeWheelContacts() const { return RuntimeWheelContacts; }
-    UFUNCTION(BlueprintPure, Category="GTT|Vehicle|Damage") float GetLastImpactSpeedKmh() const { return LastImpactSpeedKmh; }
-    UFUNCTION(BlueprintPure, Category="GTT|Vehicle|Damage") int32 GetNativeImpactCount() const { return NativeImpactCount; }
-    UFUNCTION(BlueprintPure, Category="GTT|Vehicle|Damage") EGTTRoadDamageZone GetLastImpactZone() const { return LastImpactZone; }
-    UFUNCTION(BlueprintPure, Category="GTT|Vehicle|Damage") FGTTRoadBodyDamageSnapshot GetBodyDamageSnapshot() const { return BodyDamage; }
-    UFUNCTION(BlueprintPure, Category="GTT|Vehicle|Workshop") bool NeedsNativeWorkshopService() const;
-    UFUNCTION(BlueprintPure, Category="GTT|Vehicle|Workshop") int32 GetBodyDamageRepairSurcharge() const;
-
+ AGTTRoadVehicleNativePawn(); virtual void Tick(float DeltaSeconds) override; virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override; virtual void Interact_Implementation(AActor* Interactor) override; virtual FText GetInteractionText_Implementation() const override; virtual void NotifyHit(UPrimitiveComponent*,AActor*,UPrimitiveComponent*,bool,FVector,FVector,FVector,const FHitResult&) override;
+ UFUNCTION(BlueprintCallable,Category="GTT|Chaos|Fleet") bool ConfigureAndValidateNativeRoadVehicle(FString& OutSummary); UFUNCTION(BlueprintCallable,Category="GTT|Chaos|Fleet") bool TryActivateLegacyTakeover(); UFUNCTION(BlueprintCallable,Category="GTT|Chaos|Fleet") void DeactivateLegacyTakeover(); UFUNCTION(BlueprintCallable,Category="GTT|Vehicle") void ExitNativeVehicle(); UFUNCTION(BlueprintCallable,Category="GTT|Vehicle|Cargo") virtual void SetCargoLoadFactor(float NewLoadFactor); UFUNCTION(BlueprintCallable,Category="GTT|Vehicle|Workshop") bool ApplyNativeWorkshopService();
+ UFUNCTION(BlueprintCallable,Category="GTT|Chaos|Smoke") bool ApplyDeterministicSmokeControl(float Throttle,float Steering,float Brake);
+ UFUNCTION(BlueprintPure,Category="GTT|Chaos|Fleet") bool IsNativeReady() const{return bNativeReady;} UFUNCTION(BlueprintPure,Category="GTT|Chaos|Fleet") bool IsLegacyTakeoverActive() const{return bTakeoverActive;} UFUNCTION(BlueprintPure,Category="GTT|Vehicle|Save") FName GetPersistentVehicleId() const{return NativeVehicleId;} UFUNCTION(BlueprintPure,Category="GTT|Vehicle") FText GetVehicleDisplayName() const{return NativeDisplayName;} UFUNCTION(BlueprintPure,Category="GTT|Vehicle") APawn* GetDriverPawn() const{return PreviousPawn.Get();} UFUNCTION(BlueprintPure,Category="GTT|Vehicle|Cargo") float GetCargoLoadFactor() const{return CargoLoadFactor;} UFUNCTION(BlueprintPure,Category="GTT|Chaos|Migration") FGTTRoadVehicleMigrationSnapshot GetMigrationSnapshot() const{return MigrationSnapshot;} UFUNCTION(BlueprintPure,Category="GTT|Chaos|Runtime") float GetRuntimeWheelRisk() const{return RuntimeWheelRisk;} UFUNCTION(BlueprintPure,Category="GTT|Chaos|Runtime") int32 GetRuntimeWheelContacts() const{return RuntimeWheelContacts;} UFUNCTION(BlueprintPure,Category="GTT|Vehicle|Damage") float GetLastImpactSpeedKmh() const{return LastImpactSpeedKmh;} UFUNCTION(BlueprintPure,Category="GTT|Vehicle|Damage") int32 GetNativeImpactCount() const{return NativeImpactCount;} UFUNCTION(BlueprintPure,Category="GTT|Vehicle|Damage") EGTTRoadDamageZone GetLastImpactZone() const{return LastImpactZone;} UFUNCTION(BlueprintPure,Category="GTT|Vehicle|Damage") FGTTRoadBodyDamageSnapshot GetBodyDamageSnapshot() const{return BodyDamage;} UFUNCTION(BlueprintPure,Category="GTT|Vehicle|Workshop") bool NeedsNativeWorkshopService() const; UFUNCTION(BlueprintPure,Category="GTT|Vehicle|Workshop") int32 GetBodyDamageRepairSurcharge() const;
 protected:
-    virtual void BeginPlay() override;
-    virtual float GetCargoPowerLimit(float SpeedKmh) const;
-    virtual float GetCargoSteeringLimit(float SpeedKmh) const;
-    virtual float GetImpactDamageScale() const;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GTT|Camera") TObjectPtr<USpringArmComponent> CameraBoom;
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GTT|Camera") TObjectPtr<UCameraComponent> VehicleCamera;
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GTT|Vehicle|Damage") TObjectPtr<UStaticMeshComponent> FrontDamageDebris;
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GTT|Vehicle|Damage") TObjectPtr<UStaticMeshComponent> RearDamageDebris;
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GTT|Vehicle|Damage") TObjectPtr<UStaticMeshComponent> LeftDamageDebris;
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GTT|Vehicle|Damage") TObjectPtr<UStaticMeshComponent> RightDamageDebris;
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|Chaos|Fleet") FName NativeVehicleId = NAME_None;
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|Chaos|Fleet") FText NativeDisplayName;
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|Chaos|Fleet") float FuelCapacityLiters = 45.0f;
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|Chaos|Fleet") float IdleFuelBurnPerSecond = 0.03f;
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|Chaos|Fleet") float FullThrottleFuelBurnPerSecond = 0.20f;
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GTT|Chaos|Fleet") FVector ExitOffset = FVector(0.0f, 180.0f, 70.0f);
-
+ virtual void BeginPlay() override; virtual float GetCargoPowerLimit(float SpeedKmh) const; virtual float GetCargoSteeringLimit(float SpeedKmh) const; virtual float GetImpactDamageScale() const;
+ UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="GTT|Camera") TObjectPtr<USpringArmComponent> CameraBoom; UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="GTT|Camera") TObjectPtr<UCameraComponent> VehicleCamera; UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="GTT|Vehicle|Damage") TObjectPtr<UStaticMeshComponent> FrontDamageDebris; UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="GTT|Vehicle|Damage") TObjectPtr<UStaticMeshComponent> RearDamageDebris; UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="GTT|Vehicle|Damage") TObjectPtr<UStaticMeshComponent> LeftDamageDebris; UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="GTT|Vehicle|Damage") TObjectPtr<UStaticMeshComponent> RightDamageDebris;
+ UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="GTT|Chaos|Fleet") FName NativeVehicleId=NAME_None; UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="GTT|Chaos|Fleet") FText NativeDisplayName; UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="GTT|Chaos|Fleet") float FuelCapacityLiters=45.f; UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="GTT|Chaos|Fleet") float IdleFuelBurnPerSecond=.03f; UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="GTT|Chaos|Fleet") float FullThrottleFuelBurnPerSecond=.20f; UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="GTT|Chaos|Fleet") FVector ExitOffset=FVector(0,180,70);
 private:
-    bool ValidateRigContract(FString& OutSummary) const;
-    bool ImportLegacyGameplayState(const AGTTVehicleBase* LegacyVehicle, FString& OutSummary);
-    void HandleNativeThrottle(float Value);
-    void HandleNativeSteering(float Value);
-    void SyncLegacyMirror();
-    void RuntimeAcceptanceGuard();
-    void UpdateNativeWheelRuntime(float DeltaSeconds);
-    void UpdateDamageConsequences(float DeltaSeconds);
-    void ApplyNativeImpactDamage(float ImpactSpeedKmh, EGTTRoadDamageZone Zone, const FVector& HitLocation, const FVector& NormalImpulse);
-    EGTTRoadDamageZone DetermineImpactZone(const FVector& HitLocation) const;
-    float& ResolveDamageZoneHealth(EGTTRoadDamageZone Zone);
-    float GetDamageZoneHealth(EGTTRoadDamageZone Zone) const;
-    void TryDetachDamagePanel(EGTTRoadDamageZone Zone, const FVector& HitLocation, const FVector& NormalImpulse, float ImpactSpeedKmh);
-    void ConfigureDamageDebrisLayout();
-    void RestoreNativeBodyDamage();
-    void StopNativeDriveForBreakdown();
-    static const TCHAR* DamageZoneToString(EGTTRoadDamageZone Zone);
-
-    UPROPERTY(VisibleInstanceOnly, Category="GTT|Chaos|Fleet") bool bNativeReady = false;
-    UPROPERTY(VisibleInstanceOnly, Category="GTT|Chaos|Fleet") bool bTakeoverActive = false;
-    UPROPERTY(VisibleInstanceOnly, Category="GTT|Chaos|Fleet") bool bOccupied = false;
-    UPROPERTY(VisibleInstanceOnly, Category="GTT|Chaos|Fleet") FString NativeAcceptanceSummary = TEXT("Not validated");
-    UPROPERTY(VisibleInstanceOnly, Category="GTT|Chaos|Migration") FGTTRoadVehicleMigrationSnapshot MigrationSnapshot;
-    UPROPERTY(VisibleInstanceOnly, Category="GTT|Vehicle|Cargo") float CargoLoadFactor = 0.0f;
-    UPROPERTY(VisibleInstanceOnly, Category="GTT|Chaos|Runtime") float RuntimeWheelRisk = 0.0f;
-    UPROPERTY(VisibleInstanceOnly, Category="GTT|Chaos|Runtime") int32 RuntimeWheelContacts = 0;
-    UPROPERTY(VisibleInstanceOnly, Category="GTT|Vehicle|Damage") float LastImpactSpeedKmh = 0.0f;
-    UPROPERTY(VisibleInstanceOnly, Category="GTT|Vehicle|Damage") int32 NativeImpactCount = 0;
-    UPROPERTY(VisibleInstanceOnly, Category="GTT|Vehicle|Damage") EGTTRoadDamageZone LastImpactZone = EGTTRoadDamageZone::Front;
-    UPROPERTY(VisibleInstanceOnly, Category="GTT|Vehicle|Damage") FGTTRoadBodyDamageSnapshot BodyDamage;
-
-    TWeakObjectPtr<APawn> PreviousPawn;
-    TWeakObjectPtr<AGTTVehicleBase> LegacyMirror;
-    float LastThrottleInput = 0.0f;
-    float RuntimeThrottleLimit = 1.0f;
-    float RuntimeSteeringLimit = 1.0f;
-    float RuntimeBrakeAssist = 0.0f;
-    float DamageThrottleLimit = 1.0f;
-    float DamageSteeringLimit = 1.0f;
-    float DamageSteeringBias = 0.0f;
-    float MirrorSyncAccumulator = 0.0f;
-    float TakeoverRetryAccumulator = 0.0f;
-    float RuntimeGuardAccumulator = 0.0f;
-    float WheelEvidenceAccumulator = 0.0f;
-    float DamageEvidenceAccumulator = 0.0f;
-    float LastImpactDamageTimeSeconds = -1000.0f;
-    bool bFrontPanelDetached = false;
-    bool bRearPanelDetached = false;
-    bool bLeftPanelDetached = false;
-    bool bRightPanelDetached = false;
+ bool ValidateRigContract(FString&) const; bool ImportLegacyGameplayState(const AGTTVehicleBase*,FString&); void HandleNativeThrottle(float); void HandleNativeSteering(float); void SyncLegacyMirror(); void RuntimeAcceptanceGuard(); void UpdateNativeWheelRuntime(float); void UpdateDamageConsequences(float); void ApplyNativeImpactDamage(float,EGTTRoadDamageZone,const FVector&,const FVector&); EGTTRoadDamageZone DetermineImpactZone(const FVector&) const; float& ResolveDamageZoneHealth(EGTTRoadDamageZone); float GetDamageZoneHealth(EGTTRoadDamageZone) const; void TryDetachDamagePanel(EGTTRoadDamageZone,const FVector&,const FVector&,float); void ConfigureDamageDebrisLayout(); void RestoreNativeBodyDamage(); void StopNativeDriveForBreakdown(); static const TCHAR* DamageZoneToString(EGTTRoadDamageZone);
+ UPROPERTY(VisibleInstanceOnly,Category="GTT|Chaos|Fleet") bool bNativeReady=false; UPROPERTY(VisibleInstanceOnly,Category="GTT|Chaos|Fleet") bool bTakeoverActive=false; UPROPERTY(VisibleInstanceOnly,Category="GTT|Chaos|Fleet") bool bOccupied=false; UPROPERTY(VisibleInstanceOnly,Category="GTT|Chaos|Fleet") FString NativeAcceptanceSummary=TEXT("Not validated"); UPROPERTY(VisibleInstanceOnly,Category="GTT|Chaos|Migration") FGTTRoadVehicleMigrationSnapshot MigrationSnapshot; UPROPERTY(VisibleInstanceOnly,Category="GTT|Vehicle|Cargo") float CargoLoadFactor=0; UPROPERTY(VisibleInstanceOnly,Category="GTT|Chaos|Runtime") float RuntimeWheelRisk=0; UPROPERTY(VisibleInstanceOnly,Category="GTT|Chaos|Runtime") int32 RuntimeWheelContacts=0; UPROPERTY(VisibleInstanceOnly,Category="GTT|Vehicle|Damage") float LastImpactSpeedKmh=0; UPROPERTY(VisibleInstanceOnly,Category="GTT|Vehicle|Damage") int32 NativeImpactCount=0; UPROPERTY(VisibleInstanceOnly,Category="GTT|Vehicle|Damage") EGTTRoadDamageZone LastImpactZone=EGTTRoadDamageZone::Front; UPROPERTY(VisibleInstanceOnly,Category="GTT|Vehicle|Damage") FGTTRoadBodyDamageSnapshot BodyDamage;
+ TWeakObjectPtr<APawn> PreviousPawn; TWeakObjectPtr<AGTTVehicleBase> LegacyMirror; float LastThrottleInput=0,RuntimeThrottleLimit=1,RuntimeSteeringLimit=1,RuntimeBrakeAssist=0,DamageThrottleLimit=1,DamageSteeringLimit=1,DamageSteeringBias=0,MirrorSyncAccumulator=0,TakeoverRetryAccumulator=0,RuntimeGuardAccumulator=0,WheelEvidenceAccumulator=0,DamageEvidenceAccumulator=0,LastImpactDamageTimeSeconds=-1000; bool bFrontPanelDetached=false,bRearPanelDetached=false,bLeftPanelDetached=false,bRightPanelDetached=false;
 };
-
-UCLASS(Blueprintable)
-class GTT_API AGTTRattlebackNativePawn : public AGTTRoadVehicleNativePawn
-{
-    GENERATED_BODY()
-public:
-    AGTTRattlebackNativePawn();
-protected:
-    virtual float GetImpactDamageScale() const override;
-};
-
-UCLASS(Blueprintable)
-class GTT_API AGTTMuleboxNativePawn : public AGTTRoadVehicleNativePawn
-{
-    GENERATED_BODY()
-public:
-    AGTTMuleboxNativePawn();
-protected:
-    virtual float GetCargoPowerLimit(float SpeedKmh) const override;
-    virtual float GetCargoSteeringLimit(float SpeedKmh) const override;
-    virtual float GetImpactDamageScale() const override;
-};
+UCLASS(Blueprintable) class GTT_API AGTTRattlebackNativePawn:public AGTTRoadVehicleNativePawn{GENERATED_BODY() public:AGTTRattlebackNativePawn(); protected:virtual float GetImpactDamageScale() const override;};
+UCLASS(Blueprintable) class GTT_API AGTTMuleboxNativePawn:public AGTTRoadVehicleNativePawn{GENERATED_BODY() public:AGTTMuleboxNativePawn(); protected:virtual float GetCargoPowerLimit(float) const override; virtual float GetCargoSteeringLimit(float) const override; virtual float GetImpactDamageScale() const override;};
