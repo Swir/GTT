1abe610d57b7ac40c78f0cad345a83ef41773f19
Wojciech <swirjestem@hotmail.com>
2026-09-17 15:42:23 +0200

    GTT 0.1.21: add night warden reinforcement and police handoff

--- Source/GTT/Private/Ranger/GTTRangerDirector.cpp
+++ Source/GTT/Private/Ranger/GTTRangerDirector.cpp
@@ -1,9 +1,14 @@
 #include "Ranger/GTTRangerDirector.h"
 
 #include "Core/GTTGameMode.h"
+#include "Core/GTTGameplayStatics.h"
+#include "Economy/GTTPlayerEconomyComponent.h"
 #include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
 #include "Ranger/GTTRangerPawn.h"
 #include "TimerManager.h"
+#include "Wanted/GTTWantedComponent.h"
+#include "World/GTTDayNightCycle.h"
 
 AGTTRangerDirector::AGTTRangerDirector()
 {
@@ -25,6 +30,28 @@ void AGTTRangerDirector::CleanupInvalidRangers()
     });
 }
 
+void AGTTRangerDirector::ApplyPoliceHandoff(APawn* PlayerPawn, int32 AlertLevel)
+{
+    if (!PlayerPawn || bPoliceHandoffIssued || AlertLevel < PoliceHandoffAlertLevel)
+    {
+        return;
+    }
+
+    if (UGTTWantedComponent* Wanted = UGTTGameplayStatics::FindWantedComponentForPawn(PlayerPawn))
+    {
+        Wanted->AddHeat(PoliceHandoffHeat);
+        bPoliceHandoffIssued = true;
+
+        if (UGTTPlayerEconomyComponent* Economy = UGTTGameplayStatics::FindEconomyComponentForPawn(PlayerPawn))
+        {
+            Economy->PushMessage(
+                FString::Printf(TEXT("WARDEN RADIO HANDOFF: county police notified | wildlife alert %d/3 | wanted %d/5"),
+                    AlertLevel, Wanted->GetWantedLevel()),
+                5.5f);
+        }
+    }
+}
+
 void AGTTRangerDirector::UpdateResponse()
 {
     AGTTGameMode* GameMode = GetWorld() ? GetWorld()->GetAuthGameMode<AGTTGameMode>() : nullptr;
@@ -34,8 +61,34 @@ void AGTTRangerDirector::UpdateResponse()
     }
 
     CleanupInvalidRangers();
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0);
     const int32 AlertLevel = GameMode->GetWildlifeAlertLevel();
-    const int32 DesiredRangers = AlertLevel > 0 ? FMath::Clamp(AlertLevel, 1, 2) : 0;
+    const AGTTDayNightCycle* DayNight = GameMode->GetDayNightCycle();
+    const bool bNight = DayNight && DayNight->IsNight();
+
+    if (AlertLevel <= 0)
+    {
+        bPoliceHandoffIssued = false;
+        bNightReinforcementActive = false;
+        EnforcementTier = 0;
+    }
+    else
+    {
+        bNightReinforcementActive = bNight && AlertLevel >= NightReinforcementAlertLevel;
+        EnforcementTier = AlertLevel + (bNightReinforcementActive ? 1 : 0) + (bPoliceHandoffIssued ? 1 : 0);
+        ApplyPoliceHandoff(PlayerPawn, AlertLevel);
+        EnforcementTier = AlertLevel + (bNightReinforcementActive ? 1 : 0) + (bPoliceHandoffIssued ? 1 : 0);
+    }
+
+    int32 DesiredRangers = 0;
+    if (AlertLevel > 0)
+    {
+        DesiredRangers = AlertLevel >= 3 ? 2 : 1;
+        if (bNightReinforcementActive)
+        {
+            DesiredRangers = FMath::Max(DesiredRangers, 2);
+        }
+    }
 
     while (ActiveRangers.Num() > DesiredRangers)
     {
@@ -53,7 +106,7 @@ void AGTTRangerDirector::UpdateResponse()
 
     while (ActiveRangers.Num() < DesiredRangers && RangerClass)
     {
-        const int32 Index = ActiveRangers.Num();
+        const int32 Index = FMath::Clamp(ActiveRangers.Num(), 0, 1);
         if (AGTTRangerPawn* Ranger = GetWorld()->SpawnActor<AGTTRangerPawn>(RangerClass, SpawnLocations[Index], FRotator::ZeroRotator))
         {
             ActiveRangers.Add(Ranger);
